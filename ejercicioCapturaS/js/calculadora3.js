7fcba0772993f7b39a97269f0ac25a9b9d14f246
sawyIbero <127158191+sawyIbero@users.noreply.github.com>
2023-03-27 20:04:43 -0500

    Add files via upload

---
+++ ejercicioCapturaS/js/calculadora3.js
@@ -0,0 +1,32 @@
+// Función suma
+function sumar() {
+    var num1 = parseInt(prompt("Ingrese el primer número: "));
+    var num2 = parseInt(prompt("Ingrese el segundo número: "));
+    var resultado = num1 + num2;
+    alert("El resultado de la suma es: " + resultado);
+  }
+  
+  // Función resta
+  function restar() {
+    var num1 = parseInt(prompt("Ingrese el primer número: "));
+    var num2 = parseInt(prompt("Ingrese el segundo número: "));
+    var resultado = num1 - num2;
+    alert("El resultado de la resta es: " + resultado);
+  }
+  
+  // Función multiplicar
+  function multiplicar() {
+    var num1 = parseInt(prompt("Ingrese el primer número: "));
+    var num2 = parseInt(prompt("Ingrese el segundo número: "));
+    var resultado = num1 * num2;
+    alert("El resultado de la multiplicación es: " + resultado);
+  }
+  
+  // Función dividir 
+  function dividir() {
+    var num1 = parseInt(prompt("Ingrese el primer número: "));
+    var num2 = parseInt(prompt("Ingrese el segundo número: "));
+    var resultado = num1 / num2;
+    alert("El resultado de la división es: " + resultado);
+  }
+  
